7a193745f3ae4b8a7d19e61bb9ceda59c1d42264
zuoxiupeng <zuoxiupeng@live.com>
2020-04-16 17:52:32 +0800

    保存rabbitmq集群配置 保存es

---
+++ rabbitmq-cluster/cluster-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+# Start RMQ from entry point.
+# This will ensure that environment variables passed
+# will be honored
+/usr/local/bin/docker-entrypoint.sh rabbitmq-server -detached
+
+# Do the cluster dance
+rabbitmqctl stop_app
+rabbitmqctl join_cluster rabbit@rabbitmq1
+
+# Stop the entire RMQ server. This is done so that we
+# can attach to it again, but without the -detached flag
+# making it run in the forground
+rabbitmqctl stop
+
+# Wait a while for the app to really stop
+sleep 2s
+
+# Start it
+rabbitmq-server
